380df00d4181cb6e9cd38f3ab5d5251edc3eca08
verma46 <115958974+verma46@users.noreply.github.com>
2022-10-30 20:54:48 -0400

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tanmayverma/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tanmayverma/Desktop/FLUTTER WORKSPACE/smart_device_management_frontend-main"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/tanmayverma/Desktop/FLUTTER WORKSPACE/smart_device_management_frontend-main/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/tanmayverma/Desktop/FLUTTER WORKSPACE/smart_device_management_frontend-main/.dart_tool/package_config.json"
